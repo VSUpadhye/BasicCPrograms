775ab663e5d265c93032eba44edc26dbf9e65779
Upadhye Varad Santosh <varad.exams@gmail.com>
2024-01-12 19:41:32 +0530

    Content of the file

--- test.c
+++ test.c
@@ -1,1 +1,20 @@
-Hi there
+#include <stdio.h>
+
+int main()
+{
+    int num1 = 0, num2 = 0;
+    int *ptr = NULL;
+
+    printf("Enter two numbers:\n");
+    scanf("%d%d", &num1, &num2);
+
+    ptr = &num1;
+    *ptr = 2 * num2;
+
+    ptr = &num2;
+    *ptr += num1;
+
+    printf("The values:\nnum1 = %d\nnum2 = %d\n", num1, num2);
+
+    return 0;
+}
