edfe109c14730bf342734b53d9d3262887058842
Upadhye Varad Santosh <varad.exams@gmail.com>
2024-01-13 20:14:18 +0530

    Correcting the code

--- pointer basics.c
+++ pointer basics.c
@@ -3,14 +3,18 @@
 int main()
 {
     void *ptr = NULL;
-    int num = 50;
-    char ch = 'K';
+    int num = 7;
+    char ch = 'D';
+    float value = 45.18;
 
     ptr = &num;
-    printf("The value of num = %d\n", (int *)ptr);
+    printf("The value of num = %d\n", *(int *)ptr);
 
     ptr = &ch;
-    printf("The value of ch = %c\n", (char *)ptr);
+    printf("The value of ch = %c\n", *(char *)ptr);
+
+    ptr = &value;
+    printf("The value of floating number is = %f\n", *(float *)ptr);
 
     return 0;
 }
