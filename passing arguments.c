938d1d30f537a9e125db1815b1b0d0704998774b
Upadhye Varad Santosh <varad.exams@gmail.com>
2024-01-16 19:39:44 +0530

    passing by values (no result)

---
+++ passing arguments.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void swapping(int x, int y)
+{
+    int temp = 0;
+    temp = x;
+    x = y;
+    y = temp;
+}
+
+int main()
+{
+    int a = 50, b = 100;
+
+    printf("The values before swapping:\na = %d,\nb = %d\n", a, b);
+
+    swapping(a, b);
+
+    printf("The values after swapping:\na = %d,\nb = %d\n", a, b);
+
+    return 0;
+}
