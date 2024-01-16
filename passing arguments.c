a7900647f561d84baab82432023bdf4e1ce0eac6
Upadhye Varad Santosh <varad.exams@gmail.com>
2024-01-16 20:02:30 +0530

    Correction in code (resulting)

--- passing arguments.c
+++ passing arguments.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 
-void swapping(int x, int y)
+void swapping(int *x, int *y)
 {
     int temp = 0;
-    temp = x;
-    x = y;
-    y = temp;
+    temp = *x;
+    *x = *y;
+    *y = temp;
 }
 
 int main()
 {
     int a = 50, b = 100;
 
-    printf("The values before swapping:\na = %d,\nb = %d\n", a, b);
+    printf("The values before swapping:\na = %x,\nb = %x\n", a, b);
 
-    swapping(a, b);
+    swapping(&a, &b);
 
     printf("The values after swapping:\na = %d,\nb = %d\n", a, b);
 
