e8f6ba00ab4d7a3aadf3940dd92f6cc6b2af21b1
Upadhye Varad Santosh <varad.exams@gmail.com>
2024-02-12 17:49:38 +0530

    Swapping successful

--- Challenges/EmpDatabase.c
+++ Challenges/EmpDatabase.c
@@ -10,19 +10,38 @@ struct Employee
     struct Employee *ptrNext;
 };
 
+
+void PrintEmployee(struct Employee *emp)
+{
+    //printf("Employee ID: %d\n", emp->emp_id);
+    printf("Name: %s\n", emp->name);
+    //printf("Department: %s\n", emp->department);
+    printf("Salary: %.2f\n\n", emp->salary);
+
+}
+
+void PrintAllEmployees(struct Employee *head)
+{
+    struct Employee *currentEmp = head;
+    
+    printf("Printing all employee details\n");
+    while(currentEmp != NULL)
+    {
+        
+        PrintEmployee(currentEmp);
+        currentEmp = currentEmp->ptrNext;
+        printf("printing temp value: %x\n", currentEmp);
+    }
+
+}
+
 void ReadInputs(struct Employee *headPtr, int size)
 {
     struct Employee *currentEmp;
     
-    if (headPtr == NULL)
-    {
-        printf("Memory allocation failed!\n");
-        exit(0);
-    }
-
     printf("Enter employee details:\n\n");
     //print the head in readimputs function;
-    printf("Adress inside head ptr in function: %x\n", headPtr);
+    //printf("Adress inside head ptr in function: %x\n", headPtr);
 
     currentEmp = headPtr;
 
@@ -48,13 +67,52 @@ void ReadInputs(struct Employee *headPtr, int size)
             currentEmp->ptrNext = (struct Employee *)malloc(1 * sizeof(struct Employee));
             currentEmp = currentEmp->ptrNext;
             //print new currentEmp
-            printf("Adress inside currentEmp ptr: %x\n", currentEmp);
+            //printf("Adress inside currentEmp ptr: %x\n", currentEmp);
         }
     }
 
-    free(currentEmp);
+    //free(currentEmp);
 }
 
+void SortEmployeesOnSalary(struct Employee *headPtr, int size)
+{
+    int ind1, ind2;
+    struct Employee *currentEmp = headPtr, *temp_emp;
+    struct Employee *lhs, *rhs;
+
+    for (ind1 = 0; ind1 < size - 1; ind1++)
+    {
+        for (ind2 = 0; ind2 < size - ind1 - 1; ind2++)
+        {
+            lhs = currentEmp;
+            rhs = currentEmp->ptrNext;
+
+            //if ((ptr + ind2)->salary < (ptr + ind2 + 1)->salary)
+            if (lhs->salary < rhs->salary)
+            {
+                printf("\nhead pointing to: %.2f,    current: %.2f\n", headPtr->salary, currentEmp->salary);
+                //Swap nodes
+                if (ind2 == 0)
+                {
+                    /* set the head to rhs node*/
+                    headPtr = rhs;
+                }
+                //remember rhs.next
+                temp_emp = rhs->ptrNext;
+                //rhs.next = current
+                rhs->ptrNext = currentEmp;
+                //lhs.next = remembered
+                rhs = temp_emp;
+                printf("\nhead pointing to: %.2f,    current: %.2f\n", headPtr->salary, currentEmp->salary);
+            }
+            currentEmp = rhs;//This is the problematic statement
+            printf("Current = %.2f\n", currentEmp->salary);
+        }
+    }
+}
+
+
+/*
 float GetTotalExpenditure(struct Employee *ptr, int size)
 {
     struct Employee *currentEmp;
@@ -70,23 +128,30 @@ float GetTotalExpenditure(struct Employee *ptr, int size)
     printf("\nThe total expenditure of the company is: %d\n", total_expenditure);
     return total_expenditure;
 }
-
+*/
 int main()
 {
     int no_of_emps;
-    
-    //Allocating memory for the head
-    struct Employee *ptrHead = (struct Employee *)malloc(1 * sizeof(struct Employee));
     printf("Enter the no. of employees:");
     scanf("%d", &no_of_emps);
+
+    //Allocating memory for the head
+    struct Employee *ptrHead = (struct Employee *)malloc(1 * sizeof(struct Employee));
+    if (ptrHead == NULL)
+    {
+        printf("Memory allocation failed!\n");
+        exit(0);
+    }
 
     //Reading inputs from user
     ReadInputs(ptrHead, no_of_emps);
     //print ptrHead in main:
     //printf("Adress inside head ptr in main: %x\n", ptrHead);
-    
+    SortEmployeesOnSalary(ptrHead, no_of_emps);
+
+    //PrintAllEmployees(ptrHead);
     //Getting total expenditure of company on salaries
-    printf("\nThe total expenditure of the company on salaries: %.2f\n", GetTotalExpenditure(ptrHead, no_of_emps));
+    //printf("\nThe total expenditure of the company on salaries: %.2f\n", GetTotalExpenditure(ptrHead, no_of_emps));
 
     free(ptrHead);
     return 0;
