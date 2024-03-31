871a61b57b847a2c9d52f1508b813694d9a03f93
VSUpadhye <varadsupadhye@gmail.com>
2024-03-31 09:55:49 +0530

    Modifying the code for square

--- CPP/printing_rectangle.cpp
+++ CPP/printing_rectangle.cpp
@@ -4,16 +4,16 @@ using namespace std;
 
 int main()
 {
-    int length {0}, breadth {0};
+    int length {0};
 
-    cout << "Enter the length and breadth of the rectangle: ";
-    cin >> length >> breadth;
+    cout << "Enter the length of the side of the square: ";
+    cin >> length;
 
-    for (int j = 0; j < breadth; j++)
+    for (int j = 0; j < length; j++)
     {
         for(int i = 0; i < length; i++)
         {
-            if (j == 0 || j == breadth - 1 || i == 0 || i == length - 1)
+            if (j == 0 || j == length - 1 || i == 0 || i == length - 1)
             {
                 cout << "* ";
             }
@@ -26,6 +26,5 @@ int main()
         cout << endl;
     }
     
-        
     return 0;
 }
